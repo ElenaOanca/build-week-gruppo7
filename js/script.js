3c1364df4379ee532b4e895f333cd1f5b7d99403
calogeroteresi <caloteresi@gmail.com>
2023-10-18 12:35:57 +0200

    Commit-da-scaricare

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+     const sections = document.querySelectorAll("section"); // Seleziona tutte le sezioni
+     // Funzione per nascondere tutte le sezioni tranne quella con l'indice specificato
+     function showPage(pageNumber) {
+       sections.forEach(function(section, index) {
+         if (index + 1 === pageNumber) {
+           section.classList.remove("hidden");
+         } else {
+           section.classList.add("hidden");
+         }
+          
+     });
+     }
+   
+     // Aggiungi un gestore di eventi a tutti i pulsanti "Next"
+     const nextButtons = document.querySelectorAll(".buttonLogica");
+     nextButtons.forEach(function(button, index) {
+       button.addEventListener("click", function() {
+         showPage(index + 2); // Passa all'indice successivo (inizia da 2)
+       });
+     });
+   });
+   
 // FUNZIONE TIMER /////////////////////////
 
 let timer = document.querySelector(".timer")
@@ -121,28 +144,3 @@ function controlloCheckbox(){
     });
   } 
 }
-controlloCheckbox()
-document.addEventListener("DOMContentLoaded", function() {
-     const sections = document.querySelectorAll("section"); // Seleziona tutte le sezioni
-     let timer = document.getElementById('timer')
-     // Funzione per nascondere tutte le sezioni tranne quella con l'indice specificato
-     function showPage(pageNumber) {
-       sections.forEach(function(section, index) {
-         if (index + 1 === pageNumber) {
-           section.classList.remove("hidden");
-         } else {
-           section.classList.add("hidden");
-         }
-          
-     });
-     }
-   
-     // Aggiungi un gestore di eventi a tutti i pulsanti "Next"
-     const nextButtons = document.querySelectorAll(".buttonLogica");
-     nextButtons.forEach(function(button, index) {
-       button.addEventListener("click", function() {
-         showPage(index + 2); // Passa all'indice successivo (inizia da 2)
-       });
-     });
-   });
-   
