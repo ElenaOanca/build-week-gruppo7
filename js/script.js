0e41f16b1e4139836fb7cf49a26f5dbb7d642470
calogeroteresi <caloteresi@gmail.com>
2023-10-18 12:17:29 +0200

    Secon-commit

--- js/script.js
+++ js/script.js
@@ -122,3 +122,27 @@ function controlloCheckbox(){
   } 
 }
 controlloCheckbox()
+document.addEventListener("DOMContentLoaded", function() {
+     const sections = document.querySelectorAll("section"); // Seleziona tutte le sezioni
+     let timer = document.getElementById('timer')
+     // Funzione per nascondere tutte le sezioni tranne quella con l'indice specificato
+     function showPage(pageNumber) {
+       sections.forEach(function(section, index) {
+         if (index + 1 === pageNumber) {
+           section.classList.remove("hidden");
+         } else {
+           section.classList.add("hidden");
+         }
+          
+     });
+     }
+   
+     // Aggiungi un gestore di eventi a tutti i pulsanti "Next"
+     const nextButtons = document.querySelectorAll(".buttonLogica");
+     nextButtons.forEach(function(button, index) {
+       button.addEventListener("click", function() {
+         showPage(index + 2); // Passa all'indice successivo (inizia da 2)
+       });
+     });
+   });
+   
