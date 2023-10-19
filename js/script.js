fa063a7033c7e8d84767926930d2cd01471278b4
ElenaOanca <elenaelena.oanca@gmail.com>
2023-10-19 11:58:20 +0100

    comit pop up

--- js/script.js
+++ js/script.js
@@ -1,33 +1,8 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded", function() {
-     const sections = document.querySelectorAll("section"); // Seleziona tutte le sezioni
-     // Funzione per nascondere tutte le sezioni tranne quella con l'indice specificato
-     function showPage(pageNumber) {
-       sections.forEach(function(section, index) {
-         if (index + 1 === pageNumber) {
-           section.classList.remove("hidden");
-         } else {
-           section.classList.add("hidden");
-         }
-          
-     });
-     }
-   
-     // Aggiungi un gestore di eventi a tutti i pulsanti "Next"
-     const nextButtons = document.querySelectorAll(".buttonLogica");
-     nextButtons.forEach(function(button, index) {
-       button.addEventListener("click", function() {
-         showPage(index + 2); // Passa all'indice successivo (inizia da 2)
-       });
-     });
-   });
-=======
 
 
 
 // STELLE //////////////////
 
->>>>>>> 9152320b62c09c724f7df880f65ea16a46656a9f
 
     // Recupera le stelle e l'elemento del valore di valutazione
   const stars = document.querySelectorAll(".star");
