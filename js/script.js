1938b9e45225364c18535e8ece189c0f00019321
MatiasPhoenix <matias.la19@gmail.com>
2023-10-19 17:30:05 +0200

    commenti-script-js

--- js/script.js
+++ js/script.js
@@ -109,6 +109,142 @@ document.addEventListener("DOMContentLoaded", function() {
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
+
+function erTimer(){
+  if (!stopTimer){
+    return;
+  }
+  
+var width = 400,
+  height = 400,
+  timePassed = 0,
+  timeLimit = 60;
+
+var fields = [{
+  value: timeLimit,
+  size: timeLimit,
+  update: function() {
+    return timePassed = timePassed + 1;
+  }
+}];
+
+var nilArc = d3.svg.arc()
+  .innerRadius(width / 3 - 133)
+  .outerRadius(width / 3 - 133)
+  .startAngle(0)
+  .endAngle(2 * Math.PI);
+
+var arc = d3.svg.arc()
+  .innerRadius(width / 3 - 55)
+  .outerRadius(width / 3 - 25)
+  .startAngle(0)
+  .endAngle(function(d) {
+    return ((d.value / d.size) * 2 * Math.PI);
+  });
+
+var svg = d3.select(".timer").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+var field = svg.selectAll(".field")
+  .data(fields)
+  .enter().append("g")
+  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+  .attr("class", "field");
+
+var back = field.append("path")
+  .attr("class", "path path--background")
+  .attr("d", arc);
+
+var path = field.append("path")
+  .attr("class", "path path--foreground");
+
+var label = field.append("text")
+  .attr("class", "label")
+  .attr("dy", ".35em");
+
+(function update() {
+
+  field
+    .each(function(d) {
+      d.previous = d.value, d.value = d.update(timePassed);
+    });
+
+  path.transition()
+    .ease("elastic")
+    .duration(500)
+    .attrTween("d", arcTween);
+
+  if ((timeLimit - timePassed) <= 10)
+    pulseText();
+  else
+    label
+    .text(function(d) {
+      return d.size - d.value;
+    });
+
+  if (timePassed <= timeLimit)
+    setTimeout(update, 1000 - (timePassed % 1000));
+  else
+    destroyTimer();
+
+})();
+
+function pulseText() {
+  back.classed("pulse", true);
+  label.classed("pulse", true);
+
+  if ((timeLimit - timePassed) >= 0) {
+    label.style("font-size", "90px")
+      .attr("transform", "translate(0," + +4 + ")")
+      .text(function(d) {
+        return d.size - d.value;
+      });
+  }
+
+  label.transition()
+    .ease("elastic")
+    .duration(900)
+    .style("font-size", "60px")
+    .attr("transform", "translate(0," + -10 + ")");
+}
+
+function destroyTimer() {
+  label.transition()
+    .ease("back")
+    .duration(700)
+    .style("opacity", "0")
+    .style("font-size", "5")
+    .attr("transform", "translate(0," + -40 + ")")
+    .each("end", function() {
+      field.selectAll("text").remove()
+    });
+
+  path.transition()
+    .ease("back")
+    .duration(700)
+    .attr("d", nilArc);
+
+  back.transition()
+    .ease("back")
+    .duration(700)
+    .attr("d", nilArc)
+    .each("end", function() {
+      field.selectAll("path").remove()
+    });
+}
+
+function arcTween(b) {
+  var i = d3.interpolate({
+    value: b.previous
+  }, b);
+  return function(t) {
+    return arc(i(t));
+  };
+}};
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 //PAGINA DELLE DOMANDE
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
@@ -217,10 +353,16 @@ const questions = [
 ];
 
 
+
+
 // Indice della domanda corrente (casuale)
 let currentQuestionIndex = Math.floor(Math.random() * questions.length);
 //array per conteggiare le domande a cui si è risposto 
 const countAnswer = [];
+// const panNumber = document.getElementById("numeroLoco");
+// const number = document.createElement("span");
+// panNumber.appendChild(number);
+// number === countAnswer.length;
 
 //creazione della pagina del quiz
 document.addEventListener("DOMContentLoaded", function () {
@@ -238,26 +380,28 @@ for (let index = 0; index < questions.length; index++) {
   correctAnswer.push(element);
 }
 
-
+  
   //funzione creazione domande
   function mostraDomandaCorrente() {
-
+    
+    
+    
     //svuota elementi pagina, così da caricarne una nuova
     pagina.innerHTML = "";
     qButton.innerHTML = "";
     
 
-    
+    //creazione div per posizionare la domanda
     const divDomanda = document.createElement("div");
     divDomanda.innerText = questions[currentQuestionIndex].question;
     pagina.appendChild(divDomanda);
-
+    //contorllo tipo di domanda (multiple/boolean)
     if (questions[currentQuestionIndex].type === "multiple") {
       mostraBottoniRisposte(currentQuestionIndex);
     } else {
       mostraBottoniVeroFalso(currentQuestionIndex);
     }
-
+    //controllo domanda e creazione bottoni corrispondenti
     if (currentQuestionIndex === 0) {
       const divQuestion = document.getElementById("nextQuestion");
       const avantiButton = document.createElement("button");
@@ -268,20 +412,20 @@ for (let index = 0; index < questions.length; index++) {
       avantiButton.appendChild(spanButton);
       divQuestion.appendChild(avantiButton);
       avantiButton.addEventListener("click", function () {
-
+        //arrivate a 10 domande, salta alla pagina successiva
         if (countAnswer.length === 10){
           window.location.assign("risultato.html");
         }else{
-
+          //riprende una nuova domanda, in modo ramdomico
           currentQuestionIndex = Math.floor(Math.random() * questions.length);
           let countObj = {};
           countAnswer.push(countObj);
-          //console.log(countAnswer);
           mostraDomandaCorrente();
         }
         });
       pagina.appendChild(avantiButton);
-    } else if (currentQuestionIndex){ //< questions.length - 1) {
+      //controllo domanda e creazione bottoni corrispondenti
+    } else if (currentQuestionIndex){
       const divQuestion = document.getElementById("nextQuestion");
       const avantiButton = document.createElement("button");
       avantiButton.classList.add("prossima-domanda")
@@ -290,11 +434,11 @@ for (let index = 0; index < questions.length; index++) {
       spanButton.classList.add("fas", "fa-arrow-right");
       avantiButton.appendChild(spanButton);
       divQuestion.appendChild(avantiButton)
+
       if (countAnswer.length === 10){
-        avantiButton.addEventListener("click", function() {
-          showPage(index + 2); // Passa all'indice successivo (inizia da 2)
-        });
-      }else{
+
+      }
+        else{
       avantiButton.addEventListener("click", function () {
         currentQuestionIndex = Math.floor(Math.random() * questions.length);
         let countObj = {};
@@ -302,10 +446,10 @@ for (let index = 0; index < questions.length; index++) {
         
         mostraDomandaCorrente();
       });}
-      //pagina.appendChild(avantiButton);
+      
     }
   }
-   
+   //generazione bottoni per le risposte
   function mostraBottoniRisposte(questNumber) {
     
     
@@ -317,21 +461,12 @@ for (let index = 0; index < questions.length; index++) {
       const button = document.createElement("button");
       button.innerText = risposta;
       button.classList.add("risposte");
-      // button.addEventListener("click", function(){
-      //   if (risposta === questions[questNumber].correct_answer){
-      //     let contatore = {};
-      //     contatoreGiuste.push(contatore);
-      //   }else {
-      //     let contatore = {};
-      //     contatoreSbagliate.push(contatore);
-      //   }
-      // });
       divRisposte.appendChild(button);
     }
 
     pagina.appendChild(divRisposte);
   }
-
+  //creazione bottoni risposta vero/falso
   function mostraBottoniVeroFalso(questNumber) {
    
     
@@ -344,23 +479,11 @@ for (let index = 0; index < questions.length; index++) {
     button1.innerText = "True";
     button2.innerText = "False";
 
-    button1.addEventListener("click", function(){
-      if (risposta === questions[questNumber].correct_answer){
-        let contatore = {};
-        contatoreGiuste.push(contatore);
-      }else {
-        let contatore = {};
-        contatoreSbagliate.push(contatore);
-      }
-    });
     
-
     divRisposte.appendChild(button1);
     divRisposte.appendChild(button2);
 
     pagina.appendChild(divRisposte);
   }
 });
-
-
 
